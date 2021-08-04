6c44e493bdaaf48efbaf6c720ad7575931f365b2
suzuran7053 <81237663+suzuran7053@users.noreply.github.com>
2021-08-04 16:31:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//$(document).ready(function() {});
-
-
-
 function closeNav() {
     $('#ham').css('width', '0');
 }
@@ -9,3 +5,11 @@ function closeNav() {
 function openNav() {
     $('#ham').css('width', '300px');
 }
+
+$(document).ready(function() {
+    $("#icon").hover(function(){
+        $(this).animate({height: "+10px", width: "+10px"}, slow);
+    }, function(){
+        $(this).animate({height: "-10px", width: "-10px"}, slow);  
+    })
+});
